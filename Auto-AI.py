2c8290fe8a6f42be9dddb1bc3730d2aa226c559e
안소명 <81541532+mmyeong@users.noreply.github.com>
2021-10-29 00:12:21 +0900

    Update Auto-AI.py

--- Auto-AI.py
+++ Auto-AI.py
@@ -13,7 +13,7 @@ class MyWindow(QDialog):
 
     def init_ui(self):
         self.main_layout = QVBoxLayout()
-
+        self.setWindowTitle("AI Service")
         self.frame_1 = QFrame()
         self.frame_1.setFrameShape(QFrame.Panel | QFrame.Sunken)
         self.frame_2 = QFrame()
@@ -21,28 +21,43 @@ class MyWindow(QDialog):
         self.frame_3 = QFrame()
         self.frame_3.setFrameShape(QFrame.Panel | QFrame.Sunken)
 
+
         self.layout_1 = QVBoxLayout()
         self.layout_2 = QVBoxLayout()
         self.layout_3 = QVBoxLayout()
+
         self.checkBox1 = QCheckBox("Spam Mail Delete", self)
-        #self.labelt = QLabel("네이버 자동 로그인 후 메일함에 들어가서 스팸메일 판독 후 스팸메일을 자동 삭제해주는 기능입니다.",self)
+        self.labelt1 = QLabel("It automatically deletes spam emails after automatic login to Naver and reading the mailbox spam emails.",self)
         self.checkBox2 = QCheckBox("Mail Send", self)
+        self.labelt2 = QLabel("It is a function that allows you to send mail.")
         self.checkBox3 = QCheckBox("News Summarize", self)
+        self.labelt3 = QLabel("It is a function that summarizes Naver news..")
+
+        self.checkBox1.setFont(QFont("궁서",15))
+        self.checkBox2.setFont(QFont("궁서", 15))
+        self.checkBox3.setFont(QFont("궁서", 15))
+        self.checkBox1.setStyleSheet("Color : green")
+        self.checkBox2.setStyleSheet("Color : green")
+        self.checkBox3.setStyleSheet("Color : green")
 
         self.layout_1.addWidget(self.checkBox1)
-        #self.layout_1.addWidget(self.labelt)
+        self.layout_1.addWidget(self.labelt1)
         self.layout_2.addWidget(self.checkBox2)
+        self.layout_2.addWidget(self.labelt2)
         self.layout_3.addWidget(self.checkBox3)
+        self.layout_3.addWidget(self.labelt3)
 
         self.frame_1.setLayout(self.layout_1)
         self.frame_2.setLayout(self.layout_2)
         self.frame_3.setLayout(self.layout_3)
 
-        self.spliter_1 = QSplitter(Qt.Horizontal)
+        self.spliter_1 = QSplitter(Qt.Vertical)
         self.spliter_1.addWidget(self.frame_1)
         self.spliter_1.addWidget(self.frame_2)
         self.spliter_1.addWidget(self.frame_3)
+
         self.main_layout.addWidget(self.spliter_1)
+
         self.checkBox1.stateChanged.connect(self.checkBoxState)
         self.checkBox2.stateChanged.connect(self.checkBoxState)
         self.checkBox3.stateChanged.connect(self.checkBoxState)
